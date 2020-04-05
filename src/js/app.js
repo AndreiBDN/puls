388be85f662ff6944ebf4b256e82dbdd6e59e0f9
Andrei <andrey84.b@gmail.com>
2020-04-05 21:02:33 +0200

    Created slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,16 @@
 const slider = tns({
-    container: '.my-slider',
-    items: 3,
-    slideBy: 'page',
-    autoplay: true
+    container: '.slider__puls',
+    items: 1,
+    slideBy: 1,
+    autoplay: false,
+    nav: false,
+    controls: false
 });
+
+document.querySelector('.slider__button_prev').onclick = function () {
+    slider.goTo('prev');
+};
+
+document.querySelector('.slider__button_next').onclick = function () {
+    slider.goTo('next');
+};
