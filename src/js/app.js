852cfd4c664fac214f6dc129c708d66ba264d552
Andrei <andrey84.b@gmail.com>
2020-04-06 16:37:53 +0200

    Created JS script for switch catalog uses tabs

--- src/js/app.js
+++ src/js/app.js
@@ -40,3 +40,30 @@ function activationInfo(item) {
 
 activationCard('.product__link');
 activationInfo('.product__link_back');
+
+//  CHANGE visibility of catalog
+
+function changeTabs(tabName, catalogName) {
+    let tabs = document.querySelectorAll('.' + tabName);
+    let catalog = document.querySelectorAll('.' + catalogName);
+    for (let i of tabs) {
+        i.addEventListener('click', function (e) {
+            e.preventDefault();
+            for (let x of tabs) {
+                x.classList.remove(tabName + '_active');
+            }
+            this.classList.add(tabName + '_active');
+            let dataTab = this.getAttribute('data-catalog');
+            for (let i of catalog) {
+
+                let x = i.getAttribute('id');
+                if (x == dataTab) {
+                    i.classList.add(catalogName + '_active');
+                } else {
+                    i.classList.remove(catalogName + '_active');
+                }
+            }
+        })
+    }
+}
+changeTabs('catalog__tab', 'catalog__wrapper');
